efafd10d8099bf21495928434098f3ab90618fee
Neha Rajesh <leehanseok3@gmail.com>
2024-12-23 21:35:58 +0530

    update: pages

---
+++ src/Home.tsx
@@ -0,0 +1,11 @@
+import { HeroSection } from "./components/HeroSection";
+import { OurProcess } from "./components/OurProcess";
+
+export const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <OurProcess />
+    </>
+  );
+};
