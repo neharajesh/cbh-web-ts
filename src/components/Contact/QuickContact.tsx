22d4339538d7956b34a146ef6558597d1aea1c28
Neha Rajesh <leehanseok3@gmail.com>
2025-01-04 11:15:47 +0530

    add: quick contact page

---
+++ src/components/Contact/QuickContact.tsx
@@ -0,0 +1,9 @@
+import { Section } from "../Section";
+
+export const QuickContact = () => {
+  return (
+    <Section title="Contact our Team" description="Let us know how we can help">
+      <div></div>
+    </Section>
+  );
+};
