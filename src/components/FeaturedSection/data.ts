532dcf2e333e144b7b19ac113d00d1cb9caffe26
Neha Rajesh <leehanseok3@gmail.com>
2025-01-14 20:12:24 +0530

    feat: add featured section

---
+++ src/components/FeaturedSection/data.ts
@@ -0,0 +1,43 @@
+type FeaturedSectionData = {
+  title: string;
+  description: string;
+  icon: string;
+};
+
+export const data: FeaturedSectionData[] = [
+  {
+    title: "Software Expertise",
+    description:
+      "Decades of experience in building beautiful, performant and user-friendly applications.",
+    icon: "",
+  },
+  {
+    title: "Pay for Results",
+    description:
+      "We charge a fixed price for the project, so you only pay when the product is completed.",
+    icon: "",
+  },
+  {
+    title: "Full Visibility",
+    description:
+      "You always have visibility on the status of the project. We communicate effectively and frequently.",
+    icon: "",
+  },
+  {
+    title: "Custom Solutions",
+    description: "Crafting bespoke solutions that meet your specific needs.",
+    icon: "",
+  },
+  {
+    title: "Quick Turnaround",
+    description:
+      "Meeting deadlines consistently, providing reliable services without compromising quality or efficiency.",
+    icon: "",
+  },
+  {
+    title: "Exceptional Support",
+    description:
+      "Dedicated assistance during and after project completion, ensuring client satisfaction and success. We listen.",
+    icon: "",
+  },
+];
---
+++ src/components/FeaturedSection/index.tsx
@@ -0,0 +1,9 @@
+import { Section } from "../Section";
+
+export const FeaturedSection = () => {
+  return (
+    <Section title="Why Choose Us" description="">
+      <p>sometext</p>
+    </Section>
+  );
+};
