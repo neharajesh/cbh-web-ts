ac1f2d56a4c86f7ad1e994995cf10817669cd57c
Neha Rajesh <leehanseok3@gmail.com>
2025-01-31 11:56:31 +0530

    add(featuredSection): text cards

--- src/components/FeaturedSection/data.ts
+++ src/components/FeaturedSection/data.ts
@@ -4,7 +4,45 @@ type FeaturedSectionData = {
   icon: string;
 };
 
-export const data: FeaturedSectionData[] = [
+export const featuredSectionText: FeaturedSectionData[] = [
+  {
+    title: "Software Expertise",
+    description:
+      "Decades of experience in building beautiful, performant and user-friendly applications.",
+    icon: "",
+  },
+  {
+    title: "Pay for Results",
+    description:
+      "We charge a fixed price for the project, so you only pay when the product is completed.",
+    icon: "",
+  },
+  {
+    title: "Full Visibility",
+    description:
+      "You always have visibility on the status of the project. We communicate effectively and frequently.",
+    icon: "",
+  },
+  {
+    title: "Custom Solutions",
+    description: "Crafting bespoke solutions that meet your specific needs.",
+    icon: "",
+  },
+  {
+    title: "Quick Turnaround",
+    description:
+      "Meeting deadlines consistently, providing reliable services without compromising quality or efficiency.",
+    icon: "",
+  },
+  {
+    title: "Exceptional Support",
+    description:
+      "Dedicated assistance during and after project completion, ensuring client satisfaction and success. We listen.",
+    icon: "",
+  },
+];
+
+export const featuredSectionNegative: FeaturedSectionData[] = [
   {
     title: "Software Expertise",
     description:
--- src/components/FeaturedSection/index.tsx
+++ src/components/FeaturedSection/index.tsx
@@ -1,9 +1,17 @@
 import { Section } from "../Section";
+import { featuredSectionText } from "./data";
 
 export const FeaturedSection = () => {
   return (
     <Section title="Why Choose Us" description="">
-      <p>sometext</p>
+      <div className="w-full flex flex-wrap">
+        {featuredSectionText.map((item) => (
+          <div className={`w-[30%] rounded-sm shadow-md m-4 p-4`}>
+            <p className="text-lg font-bold">{item.title}</p>
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </div>
     </Section>
   );
 };
